6025a97ec12d5a717267975157633aa524bc4aa2
Utkarsh Tyagi <utkarshtyagi1612@gmail.com>
2025-10-29 12:58:29 +0530

    Day 3

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-gsap.to("#page2 h1",{
-    transform: "translate(-90%)",
-    scrollTrigger:{
-        trigger:"#page2",
-        scroller: "body",
-        markers: true,
-        start: "top 0%",
-        end: "top -100%",
-        scrub: 2,
-        pin: true,
-    }
+let path = `M 20 100 Q 768 100 1516 100`
+
+let finalPath = `M 20 100 Q 768 100 1516 100`
+
+let string = document.querySelector("#string")
+
+string.addEventListener("mousemove",function(dets){
+    path = `M 20 100 Q ${dets.x} ${dets.y} 1516 100`
+    gsap.to("svg path",{
+        attr:{ d: path },
+        duration: 1,
+        ease: "elastic.out"
+    })
+    console.log(path)
 })
+
+string.addEventListener("mouseleave",function(){
+    gsap.to("svg path",{
+        attr: {d: finalPath},
+        duration: 1.5,
+        ease: "elastic.out(1,0.2)"
+    })
+})
+
